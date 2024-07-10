4e9798f92de726e68fb6608289f513650e473c82
KoalaTai <129363392+KoalaTai@users.noreply.github.com>
2024-07-09 19:23:22 -0500

    Update index.tsx for home page

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import ComprehensiveAuditCoach from '../components/ComprehensiveAuditCoach';
+
+export default function Home() {
+  return (
+    <div className=\"container mx-auto px-4 py-7\">
+      <ComprehensiveAuditCoach />
+    </div>
+  );
+}
